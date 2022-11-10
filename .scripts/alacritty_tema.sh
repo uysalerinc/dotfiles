846d719bff659206ed8561fd4cb0e3a46214ea86
uysalerinc <erincuysal@outlook.com>
2022-11-10 22:21:56 +0300

    first commit

---
+++ .scripts/alacritty_tema.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+my_array=($(ls ~/.config/alacritty | tr " " "\n"))
+my_array=( ${my_array[@]/alacritty.yml})
+len=${#my_array[*]}
+count=1
+
+for (( i=0; i<${len}; i++ ));
+do
+  echo "$count  ${my_array[$i]}"
+  ((count++))
+done
+
+echo "Choose new theme"
+read -r new_theme
+let "new_theme-=1"
+echo "${my_array[$new_theme]}"
+rm ~/.config/alacritty/alacritty.yml && cp ~/.config/alacritty/${my_array[$new_theme]} ~/.config/alacritty/alacritty.yml
